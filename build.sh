39cb0587dd7d005d2aac52fe83d539d6605439f0
joe-butler2 <joebutler2@protonmail.com>
2021-08-18 17:30:03 +0100

    removed build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-bundle exec jekyll build --incremental -d ../joe-butler2.github.io
-git add .
-read -p "Enter websiteFiles commit message: " message
-git commit -m "$message"
-git push origin master
-(cd ../joe-butler2.github.io ; git add . && read -p "Enter joe-butler2.github.io commit message: " message2 ; git commit -m "$message2" && git push origin master)
